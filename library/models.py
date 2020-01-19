e0a76a330be16a18922bf29a39bb5653b7ee1b4c
Benjamin Eskola <ben@eskola.uk>
2020-01-19 21:33:28 +0000

    Add initial models

--- library/models.py
+++ library/models.py
@@ -1,3 +1,71 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Author(models.Model):
+    surname = models.CharField(max_length=255)
+    forenames = models.CharField(max_length=255)
+
+    class Gender(models.IntegerChoices):
+        UNKNOWN = 0
+        MALE = 1
+        FEMALE = 2
+        ORGANIZATION = 3
+
+    gender = models.IntegerField(choices=Gender.choices, default=0)
+
+    def __str__(self):
+        return f"{self.surname}, {self.forenames}"
+
+    @property
+    def initials(self):
+        return ".".join([name[0] for name in self.forenames.split(" ")]) + "."
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    subtitle = models.CharField(max_length=255, blank=True)
+    authors = models.ManyToManyField(Author, through="BookAuthor", related_name="books")
+
+    class Format(models.IntegerChoices):
+        PAPERBACK = 1
+        HARDBACK = 2
+        EBOOK = 3
+        WEB = 4
+
+    first_published = models.PositiveSmallIntegerField(blank=True, null=True)
+    language = models.CharField(max_length=2, default="en")
+
+    # these at least in theory relate only to an edition, not every edition
+    # edition could be a separate models but it would almost always be one-to-one
+    edition_published = models.PositiveSmallIntegerField(blank=True, null=True)
+    publisher = models.CharField(max_length=255, blank=True)
+    edition_format = models.CharField(
+        max_length=255, choices=Format.choices, blank=True
+    )
+    edition_number = models.PositiveSmallIntegerField(blank=True, null=True)
+    page_count = models.PositiveSmallIntegerField(blank=True, null=True)
+    goodreads_id = models.CharField(max_length=255, blank=True)
+    google_books_id = models.CharField(max_length=255, blank=True)
+    isbn = models.CharField(max_length=13, blank=True)
+    asin = models.CharField(max_length=255, blank=True)
+    edition_language = models.CharField(max_length=2, blank=True)  # i.e., a translation
+    edition_title = models.CharField(max_length=255, blank=True)  # if translated
+    edition_subtitle = models.CharField(max_length=255, blank=True)  # if translated
+
+    owned = models.BooleanField(default=False)
+    acquired_date = models.DateField(blank=True, null=True)
+    alienated_date = models.DateField(blank=True, null=True)
+    was_borrowed = models.BooleanField(default=False)
+    borrowed_from = models.CharField(max_length=255, blank=True)
+
+    image_url = models.URLField(blank=True)
+    publisher_url = models.URLField(blank=True)
+
+
+class BookAuthor(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    role = models.CharField(max_length=255)
+    order = models.PositiveSmallIntegerField(blank=True, null=True)
