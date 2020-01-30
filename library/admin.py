7c6724f26cf82d25b941a9e16dea58154f2a7704
Benjamin Eskola <ben@eskola.uk>
2020-01-30 11:05:06 +0000

    Put bookauthor on book admin but not author admin

--- library/admin.py
+++ library/admin.py
@@ -11,15 +11,11 @@ class BookAuthorInline(admin.TabularInline):
     extra = 1
 
 
-class AuthorAdmin(admin.ModelAdmin):
-    inlines = (BookAuthorInline,)
-
-
 class BookAdmin(admin.ModelAdmin):
     inlines = (BookAuthorInline,)
 
 
-admin.site.register(Author, AuthorAdmin)
+admin.site.register(Author)
 admin.site.register(Book, BookAdmin)
 admin.site.register(BookAuthor)
 admin.site.register(LogEntry)
