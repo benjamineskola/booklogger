ac8e3c58de1ffcccebcb2ba9e5696c187e1b93ec
Benjamin Eskola <ben@eskola.uk>
2020-01-30 08:06:16 +0000

    Put authors in the book admin page

--- library/admin.py
+++ library/admin.py
@@ -1,11 +1,25 @@
 from django.contrib import admin
+from django.db import models
 
 from .models import Author, Book, BookAuthor, LogEntry
 
 # Register your models here.
 
 
-admin.site.register(Author)
-admin.site.register(Book)
+class BookAuthorInline(admin.TabularInline):
+    model = BookAuthor
+    extra = 1
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    inlines = (BookAuthorInline,)
+
+
+class BookAdmin(admin.ModelAdmin):
+    inlines = (BookAuthorInline,)
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
 admin.site.register(BookAuthor)
 admin.site.register(LogEntry)
