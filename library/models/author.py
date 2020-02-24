41e8b55edf5b8ca91386aa3e8054f410c2f8123f
Benjamin Eskola <ben@eskola.uk>
2020-02-24 14:24:45 +0000

    annotate author

--- library/models/author.py
+++ library/models/author.py
@@ -1,4 +1,5 @@
 import re
+from typing import Any, Dict, Iterable, Optional
 
 from django.contrib.postgres.search import TrigramDistance
 from django.db import models
@@ -7,9 +8,11 @@ from django.db.models.functions import Lower
 from django.db.models.indexes import Index
 from django.urls import reverse
 
+Book = models.Model
 
-class AuthorManager(models.Manager):
-    def search(self, pattern):
+
+class AuthorManager(models.Manager):  # type: ignore
+    def search(self, pattern: str) -> "models.QuerySet[Author]":
         return Author.objects.annotate(
             sn_distance=TrigramDistance("surname", pattern),
             fn_distance=TrigramDistance("forenames", pattern),
@@ -40,7 +43,7 @@ class Author(models.Model):
     gender = models.IntegerField(choices=Gender.choices, default=0)
     poc = models.BooleanField(default=False)
 
-    def __str__(self):
+    def __str__(self) -> str:
         if not self.forenames:
             return self.surname
         elif self.surname_first:
@@ -49,22 +52,24 @@ class Author(models.Model):
             return self.forenames + " " + self.surname
 
     @property
-    def name_with_initials(self):
+    def name_with_initials(self) -> str:
         if self.forenames:
             return self.surname + ", " + self.initials
         else:
             return self.surname
 
-    def get_absolute_url(self):
+    def get_absolute_url(self) -> str:
         return reverse("library:author_details", args=[str(self.id)])
 
-    def get_link_data(self, book=None, **kwargs):
+    def get_link_data(
+        self, book: Optional[Book] = None, **kwargs: Dict[str, Any]
+    ) -> Dict[str, str]:
         return {
             "url": self.get_absolute_url(),
             "text": self.attribution_for(book, initials=False) if book else str(self),
         }
 
-    def attribution_for(self, book, initials=False):
+    def attribution_for(self, book: "Book", initials: bool = False) -> str:
         role = self._role_for_book(book)
         if initials:
             name = self.name_with_initials
@@ -72,30 +77,33 @@ class Author(models.Model):
             name = str(self)
         return name + (f" ({role})" if role else "")
 
-    def _role_for_book(self, book):
-        if book.first_author == self:
-            return (
-                "ed." if book.first_author_role == "editor" else book.first_author_role
-            )
-        elif rel := self.bookauthor_set.get(book=book.id):
-            return rel.display_role
+    def _role_for_book(self, book: "Book") -> str:
+        if book.first_author == self:  # type: ignore [attr-defined]
+            if book.first_author_role == "editor":  # type: ignore [attr-defined]
+                return "ed."
+            else:
+                return str(book.first_author_role)  # type: ignore [attr-defined]
+        elif rel := self.bookauthor_set.get(book=book.id):  # type: ignore [attr-defined]
+            return str(rel.display_role)
+        else:
+            return ""
 
     @property
-    def initials(self):
+    def initials(self) -> str:
         if not self.forenames:
             return ""
         all_forenames = re.split(r"[. ]+", self.forenames)
         return ".".join([name[0] for name in all_forenames if name]) + "."
 
     @property
-    def books(self):
+    def books(self) -> "models.QuerySet[Book]":
         return (
             self.first_authored_books.all() | self.additional_authored_books.all()
         ).distinct()
 
     @property
-    def authored_books(self):
-        books = (
+    def authored_books(self) -> "models.QuerySet[Book]":
+        books: "models.QuerySet[Book]" = (
             self.books.filter(
                 id__in=[
                     ba.book.id
@@ -109,8 +117,8 @@ class Author(models.Model):
         return books
 
     @property
-    def edited_books(self):
-        books = (
+    def edited_books(self) -> "models.QuerySet[Book]":
+        books: "models.QuerySet[Book]" = (
             self.books.filter(
                 id__in=[ba.book.id for ba in self.bookauthor_set.filter(role="editor")]
             )
