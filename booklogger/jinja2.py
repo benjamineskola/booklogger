fbf940089d4ac77c3ceedb23cf8fd2f4b0b33957
Benjamin Eskola <ben@eskola.uk>
2020-06-28 10:29:35 +0100

    Render reviews as markdown

--- booklogger/jinja2.py
+++ booklogger/jinja2.py
@@ -1,7 +1,8 @@
+import commonmark
 from django.templatetags.static import static
 from django.urls import reverse
 
-from jinja2 import Environment
+from jinja2 import Environment, Markup
 from library.utils import get_hyperlink, oxford_comma
 
 
@@ -9,4 +10,6 @@ def environment(**options):
     env = Environment(**options)
     env.globals.update({"static": static, "url": reverse})
     env.filters.update({"oxford_comma": oxford_comma, "get_hyperlink": get_hyperlink})
+
+    env.filters["markdown"] = lambda text: Markup(commonmark.commonmark(text))
     return env
