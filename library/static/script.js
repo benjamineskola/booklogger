904d7e7abdcd1c3278ccd7771e052bc2277781d1
Benjamin Eskola <ben@eskola.uk>
2020-07-07 13:37:35 +0100

    AJAXify the tag addition form

---
+++ library/static/script.js
@@ -0,0 +1,32 @@
+$(document).ready(function () {
+  $(".form-addtag").each(function (i, form) {
+    $(form).submit(function (event) {
+      event.preventDefault();
+      var form = $(this);
+      var url = form.attr("action");
+
+      $.ajax({
+        type: "POST",
+        url: url,
+        data: form.serialize(),
+        success: function (data) {
+          var tags_field = $(form.data("tags"));
+          var input_field = form.find('input[name="tags"]');
+          var new_tags = input_field.val().split(",");
+          for (var i in new_tags) {
+            new_tag = new_tags[i].trim();
+            tags_field.prepend(
+              '<a href="/tag/' +
+                new_tag +
+                '" class="badge badge-secondary">' +
+                new_tag +
+                "</a> "
+            );
+          }
+          input_field.val("");
+          $(form).collapse("hide");
+        },
+      });
+    });
+  });
+});
