af7bd8e79ee5972a12439a15bfda2e1390c2d574
Benjamin Eskola <ben@eskola.uk>
2020-07-10 14:57:46 +0100

    Report books not matching author's primary language

--- library/views/__init__.py
+++ library/views/__init__.py
@@ -6,7 +6,7 @@ from random import shuffle
 import requests
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.db.models import Count, Q
+from django.db.models import Count, F, Q
 from django.shortcuts import get_object_or_404, redirect, render
 from django.utils import timezone
 
@@ -289,6 +289,10 @@ def report(request, page=None):
             lambda: Book.objects.filter(
                 borrowed_from="public domain", publisher_url=""
             ),
+        ),
+        (
+            "First editions recorded as English for non-English authors",
+            lambda: Book.objects.exclude(language=F("first_author__primary_language")),
         ),
     ]
 
