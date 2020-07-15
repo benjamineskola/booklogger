28f17a2ee6fc7b2468e82fb8e4bf5410c4bbcee4
Benjamin Eskola <ben@eskola.uk>
2020-07-15 10:45:45 +0100

    Allow filtering authors by gender/ethnicity

--- library/views/author.py
+++ library/views/author.py
@@ -21,10 +21,35 @@ class IndexView(generic.ListView):
     model = Author
     paginate_by = 100
 
+    def get_queryset(self):
+        qs = super().get_queryset()
+        if gender := self.request.GET.get("gender"):
+            if not gender.isnumeric():
+                gender = Author.Gender[gender.upper()]
+                print(gender)
+            qs = qs.filter(gender=gender)
+        if poc := self.request.GET.get("poc"):
+            if poc.lower() in ["1", "true"]:
+                qs = qs.filter(poc=True)
+            elif poc.lower() in ["0", "false"]:
+                qs = qs.filter(poc=False)
+
+        return qs
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["total_authors"] = self.get_queryset().count()
         context["page_title"] = "Authors"
+        if gender := self.request.GET.get("gender"):
+            if gender.isnumeric():
+                gender = Author.Gender.choices[int(gender)][1].lower()
+            context["gender"] = gender
+        if poc := self.request.GET.get("poc"):
+            if poc.lower() in ["1", "true"]:
+                context["poc"] = "poc"
+            elif poc.lower() in ["0", "false"]:
+                context["poc"] = "white"
+
         return context
 
 
