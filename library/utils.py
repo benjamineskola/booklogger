394ed88751a81b0fb19b16e4bee38a570ceef165
Benjamin Eskola <ben@eskola.uk>
2020-07-16 09:39:08 +0100

    Hardcode languages so that mypy can be reenabled

--- library/utils.py
+++ library/utils.py
@@ -2,17 +2,9 @@ from typing import Any, List
 
 from django.conf import settings
 
-LANGUAGES = dict(
-    set(
-        [
-            (l, y.split(" ", 1)[-1])
-            for x, y in settings.LANGUAGES
-            if len(l := x.split("-", 1)[0]) == 2
-        ]
-    ),
-)
-LANGUAGES["nb"] = "Norwegian"
-LANGUAGES = sorted(LANGUAGES.items(), key=lambda x: x[1])
+# fmt: off
+LANGUAGES = {"af": "Afrikaans", "sq": "Albanian", "ar": "Arabic", "hy": "Armenian", "az": "Azerbaijani", "eu": "Basque", "be": "Belarusian", "bn": "Bengali", "bs": "Bosnian", "br": "Breton", "bg": "Bulgarian", "my": "Burmese", "ca": "Catalan", "zh": "Chinese", "hr": "Croatian", "cs": "Czech", "da": "Danish", "nl": "Dutch", "en": "English", "eo": "Esperanto", "et": "Estonian", "fi": "Finnish", "fr": "French", "fy": "Frisian", "gd": "Gaelic", "gl": "Galician", "ka": "Georgian", "de": "German", "el": "Greek", "he": "Hebrew", "hi": "Hindi", "hu": "Hungarian", "is": "Icelandic", "io": "Ido", "id": "Indonesian", "ia": "Interlingua", "ga": "Irish", "it": "Italian", "ja": "Japanese", "kn": "Kannada", "kk": "Kazakh", "km": "Khmer", "ko": "Korean", "sr": "Latin", "lv": "Latvian", "lt": "Lithuanian", "lb": "Luxembourgish", "mk": "Macedonian", "ml": "Malayalam", "mr": "Marathi", "mn": "Mongolian", "ne": "Nepali", "nb": "Norwegian", "nn": "Nynorsk", "os": "Ossetic", "fa": "Persian", "pl": "Polish", "pt": "Portuguese", "pa": "Punjabi", "ro": "Romanian", "ru": "Russian", "sk": "Slovak", "sl": "Slovenian", "es": "Spanish", "sw": "Swahili", "sv": "Swedish", "ta": "Tamil", "tt": "Tatar", "te": "Telugu", "th": "Thai", "tr": "Turkish", "uk": "Ukrainian", "ur": "Urdu", "uz": "Uzbek", "vi": "Vietnamese", "cy": "Welsh"}.items()  # noqa: B950
+# fmt: on
 
 
 def oxford_comma(items: List[str]) -> str:
