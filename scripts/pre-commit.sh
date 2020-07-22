80c5a95e77073c2c0708b3b886b0d4b858a1b04a
Benjamin Eskola <ben@eskola.uk>
2020-07-22 10:19:11 +0100

    Use find/curlylint rather than letting curlylint walk subdirectories
    Recursing subdirectories with a symlink loop causes the fan to spin up
    like mad

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-curlylint library/jinja2/
+find library/jinja2/ -type f -exec curlylint --rule 'indent: 2' {} +
 flake8 library
 pytest -q
 mypy --show-error-codes -m library.models -m library.utils
