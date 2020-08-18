6fae300b4e80374f7114257db4c13eee631751f6
Benjamin Eskola <ben@eskola.uk>
2020-08-18 18:23:13 +0100

    Wrap mypy in pipenv run

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -3,4 +3,4 @@ set -e
 find library/jinja2/ -type f -exec curlylint --rule 'indent: 2' {} +
 flake8 library
 pytest -q
-mypy --show-error-codes -m library.models -m library.utils
+pipenv run mypy --show-error-codes -m library.models -m library.utils
