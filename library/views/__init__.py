a959e5eed007a28827a651a242748bb191233466
Benjamin Eskola <ben@eskola.uk>
2020-09-08 10:10:23 +0100

    Separate out search view

--- library/views/__init__.py
+++ library/views/__init__.py
@@ -1,37 +1,10 @@
-from django.db.models import Q
 from django.shortcuts import redirect, render
 from django.utils import timezone
+from library.models import Book, LogEntry
 
-from library.models import Author, Book, LogEntry
-
-from . import author, book, importer, report, series  # noqa: F401
+from . import author, book, importer, report, search, series  # noqa: F401
 
 # Create your views here.
-
-
-def basic_search(request):
-    query = request.GET.get("query")
-
-    authors = []
-    books = []
-    if query:
-        if (
-            books := Book.objects.filter(Q(isbn=query) | Q(asin=query))
-        ) and books.count() == 1:
-            return redirect(books[0])
-        books = Book.objects.search(query)
-        authors = Author.objects.search(query).filter(similarity__gt=0.25)
-
-    return render(
-        request,
-        "search.html",
-        {
-            "page_title": f"Search{': ' + query if query else ''}",
-            "authors": authors,
-            "books": books,
-            "query": query,
-        },
-    )
 
 
 def tag_cloud(request):
---
+++ library/views/search.py
@@ -0,0 +1,28 @@
+from django.db.models import Q
+from django.shortcuts import redirect, render
+from library.models import Author, Book
+
+
+def basic_search(request):
+    query = request.GET.get("query")
+
+    authors = []
+    books = []
+    if query:
+        if (
+            books := Book.objects.filter(Q(isbn=query) | Q(asin=query))
+        ) and books.count() == 1:
+            return redirect(books[0])
+        books = Book.objects.search(query)
+        authors = Author.objects.search(query).filter(similarity__gt=0.25)
+
+    return render(
+        request,
+        "search.html",
+        {
+            "page_title": f"Search{': ' + query if query else ''}",
+            "authors": authors,
+            "books": books,
+            "query": query,
+        },
+    )
