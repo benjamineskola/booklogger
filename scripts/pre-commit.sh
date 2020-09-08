edffac4b07f30295659b962f39ba033238c5bccc
Benjamin Eskola <ben@eskola.uk>
2020-09-08 10:12:05 +0100

    Run precommit hook through pipenv

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-find library/jinja2/ -type f -exec curlylint --rule 'indent: 2' {} +
-flake8 library
-pytest -q
+pipenv run find library/jinja2/ -type f -exec curlylint --rule 'indent: 2' {} +
+pipenv run flake8 library
+pipenv run pytest -q
 pipenv run mypy --show-error-codes -m library.models -m library.utils
