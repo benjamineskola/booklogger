b3cfb0e533d73a4c324113af6918ec1beb7272cb
Benjamin Eskola <ben@eskola.uk>
2020-12-04 12:39:27 +0000

    Handle slashes in series and publisher

--- library/views/book.py
+++ library/views/book.py
@@ -130,28 +130,30 @@ class SeriesIndexView(IndexView):
 
     def get_queryset(self):
         books = super().get_queryset()
-        books = books.filter(series=self.kwargs["series"])
+        self.series = self.kwargs["series"].replace("%2f", "/")
+        books = books.filter(series=self.series)
         return books
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context[
             "page_title"
-        ] = f"{self.kwargs['series']} Series ({len(context['page_obj'])} books)"
+        ] = f"{self.series} Series ({len(context['page_obj'])} books)"
         return context
 
 
 class PublisherIndexView(IndexView):
     def get_queryset(self):
         books = super().get_queryset()
-        books = books.filter(publisher=self.kwargs["publisher"])
+        self.publisher = self.kwargs["publisher"].replace("%2f", "/")
+        books = books.filter(publisher=self.publisher)
         return books
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context[
             "page_title"
-        ] = f"Books published by {self.kwargs['publisher']} ({self.get_queryset().count()} books)"
+        ] = f"Books published by {self.publisher} ({self.get_queryset().count()} books)"
         return context
 
 
