dee447ee0eea292f9109232912ab3fcdf02952a4
Benjamin Eskola <ben@eskola.uk>
2020-12-09 14:16:45 +0000

    Fix script events for lazy-loaded pages

--- library/static/script.js
+++ library/static/script.js
@@ -1,115 +1,8 @@
 $(document).ready(function () {
-  $(".form-addtag").submit(function (event) {
-    event.preventDefault();
-    var form = $(this);
-    var url = form.attr("action");
-
-    $.ajax({
-      type: "POST",
-      url: url,
-      data: form.serialize(),
-      dataType: "json",
-      success: function (data) {
-        var tags_field = $(form.data("tags"));
-        var input_field = form.find('input[name="tags"]');
-        for (var i in data.tags) {
-          new_tag = data.tags[i];
-          tags_field.prepend(
-            `<span class="badge badge-secondary"><a href="/tag/${new_tag}">${new_tag}</a></span> `
-          );
-        }
-        input_field.val("");
-      },
-    });
-  });
-
-  $("form.update-progress").submit(function (event) {
-    event.preventDefault();
-    var form = $(this);
-    var url = form.attr("action");
-    var book = form.data("book");
-
-    $.ajax({
-      type: "POST",
-      url: url,
-      data: form.serialize(),
-      dataType: "json",
-      success: function (data) {
-        var progress_bar = $(`#book-${book} .progress-bar`);
-        progress_bar.css("width", `${data["percentage"]}%`);
-
-        var progress_text = $(`#book-${book} .card-footer .progress-date`);
-        if (progress_text.length == 0) {
-          var progress_text = $(`#book-${book} .card-footer .read-dates`);
-          progress_text.text(
-            `${progress_text.text()}; ${data["progress_text"]}`
-          );
-        } else {
-          progress_text.text(data["progress_text"]);
-        }
-
-        form.find('input[name="value"]').val("");
-      },
-    });
-  });
-
-  $(".rating-star").on("click", function () {
-    var value = $(this).data("rating");
-    var ratings = $(this).parent();
-    var book = ratings.data("book");
-    var old_rating = ratings.data("rating");
-
-    if (old_rating == value) {
-      value = value - 0.5;
-    } else if (old_rating == value - 0.5) {
-      value = 0;
-    }
-
-    $.ajax({
-      type: "POST",
-      url: `/book/${book}/rate/`,
-      data: { rating: Number(value) },
-      beforeSend: function (xhr, settings) {
-        xhr.setRequestHeader(
-          "X-CSRFToken",
-          $("[name=csrfmiddlewaretoken]").val()
-        );
-      },
-      success: function (data) {
-        ratings.data("rating", value);
-        ratings.children().each(function (i, star) {
-          if (value >= i + 1) {
-            $(star).text("★");
-          } else if (value - i == 0.5) {
-            $(star).text("½");
-          } else {
-            $(star).text("☆");
-          }
-        });
-      },
-    });
-  });
-
-  $("a.remove-tag").click(function () {
-    var tag = $(this).data("tag");
-    var label = $(this).parent();
-    var book = label.parent().data("book");
-    $.ajax({
-      type: "POST",
-      url: `/book/${book}/remove_tags/`,
-      data: { tags: tag },
-      beforeSend: function (xhr, settings) {
-        xhr.setRequestHeader(
-          "X-CSRFToken",
-          $("[name=csrfmiddlewaretoken]").val()
-        );
-        return confirm(`Remove tag ${tag}?`);
-      },
-      success: function (data) {
-        label.remove();
-      },
-    });
-  });
+  $("a.remove-tag").on("click", remove_tag);
+  $("form.add-tag").on("submit", add_tag);
+  $("form.update-progress").on("submit", update_progress);
+  $("span.rating-star").on("click", rate_book);
 
   document.years_loading = 0;
   $("div.stats-for-year").each(function (i, e) {
@@ -131,6 +24,11 @@ function load_next_page(year, url) {
       var body = $(data).find(".book.card").parent().parent().parent();
       body.insertAfter(placeholder.parent());
       placeholder.remove();
+
+      body.find("a.remove-tag").on("click", remove_tag);
+      body.find("form.add-tag").on("submit", add_tag);
+      body.find("form.update-progress").on("submit", update_progress);
+      body.find("span.rating-star").on("click", rate_book);
     },
     error: function () {
       placeholder.find(".spinner-grow").addClass("d-none");
@@ -178,6 +76,117 @@ function load_stats_for_year(i, e, update_counts) {
           $("#loading-stats").remove();
         }
       }
+    },
+  });
+}
+
+function add_tag(event) {
+  event.preventDefault();
+  var form = $(this);
+  var url = form.attr("action");
+
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: form.serialize(),
+    dataType: "json",
+    success: function (data) {
+      var tags_field = $(form.data("tags"));
+      var input_field = form.find('input[name="tags"]');
+      for (var i in data.tags) {
+        new_tag = data.tags[i];
+        tags_field.prepend(
+          `<span class="badge badge-secondary"><a href="/tag/${new_tag}">${new_tag}</a></span> `
+        );
+      }
+      input_field.val("");
+    },
+  });
+}
+
+function rate_book() {
+  console.log(this);
+  var value = $(this).data("rating");
+  var ratings = $(this).parent();
+  var book = ratings.data("book");
+  var old_rating = ratings.data("rating");
+
+  if (old_rating == value) {
+    value = value - 0.5;
+  } else if (old_rating == value - 0.5) {
+    value = 0;
+  }
+
+  $.ajax({
+    type: "POST",
+    url: `/book/${book}/rate/`,
+    data: { rating: Number(value) },
+    beforeSend: function (xhr, settings) {
+      xhr.setRequestHeader(
+        "X-CSRFToken",
+        $("[name=csrfmiddlewaretoken]").val()
+      );
+    },
+    success: function (data) {
+      ratings.data("rating", value);
+      ratings.children().each(function (i, star) {
+        if (value >= i + 1) {
+          $(star).text("★");
+        } else if (value - i == 0.5) {
+          $(star).text("½");
+        } else {
+          $(star).text("☆");
+        }
+      });
+    },
+  });
+}
+
+function remove_tag() {
+  var tag = $(this).data("tag");
+  var label = $(this).parent();
+  var book = label.parent().data("book");
+  $.ajax({
+    type: "POST",
+    url: `/book/${book}/remove_tags/`,
+    data: { tags: tag },
+    beforeSend: function (xhr, settings) {
+      xhr.setRequestHeader(
+        "X-CSRFToken",
+        $("[name=csrfmiddlewaretoken]").val()
+      );
+      return confirm(`Remove tag ${tag}?`);
+    },
+    success: function (data) {
+      label.remove();
+    },
+  });
+}
+
+function update_progress(event) {
+  event.preventDefault();
+  var form = $(this);
+  var url = form.attr("action");
+  var book = form.data("book");
+
+  $.ajax({
+    type: "POST",
+    url: url,
+    data: form.serialize(),
+    dataType: "json",
+    success: function (data) {
+      var progress_bar = $(`#book-${book} .progress-bar`);
+      progress_bar.css("width", `${data["percentage"]}%`);
+
+      var progress_text = $(`#book-${book} .card-footer .progress-date`);
+      if (progress_text.length == 0) {
+        var progress_text = $(`#book-${book} .card-footer .read-dates`);
+        progress_text.text(`${progress_text.text()}; ${data["progress_text"]}`);
+      } else {
+        progress_text.text(data["progress_text"]);
+      }
+
+      form.find('input[name="value"]').val("");
     },
   });
 }
