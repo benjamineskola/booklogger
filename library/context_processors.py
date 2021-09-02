f2a0cdea069ff5e9a7529b4244013e9cc11ae216
Benjamin Eskola <ben@eskola.uk>
2021-09-02 10:48:21 +0100

    Fix type errors in library.context_processors

--- library/context_processors.py
+++ library/context_processors.py
@@ -1,11 +1,10 @@
 import os
 import time
+from typing import Any, Dict
 
 
-def export_vars(request):
+def export_vars(request: Any) -> Dict[str, str]:
     return {
-        "VERSION_NUMBER": os.environ["VERSION_NUMBER"]
-        if "VERSION_NUMBER" in os.environ
-        else str(int(time.time())),
-        "COMMIT_ID": os.environ.get("COMMIT_ID"),
+        "VERSION_NUMBER": os.environ.get("VERSION_NUMBER", str(int(time.time()))),
+        "COMMIT_ID": os.environ.get("COMMIT_ID", "0000000"),
     }
