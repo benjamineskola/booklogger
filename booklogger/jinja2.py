4ba11d706dde525320f19c72f51b448ecbbaa996
Benjamin Eskola <ben@eskola.uk>
2021-09-03 09:28:35 +0100

    Fix type errors in booklogger.jinja2

--- booklogger/jinja2.py
+++ booklogger/jinja2.py
@@ -1,17 +1,18 @@
 import json
 import urllib.parse
+from typing import Any
 
 import commonmark
 from crispy_forms.templatetags.crispy_forms_filters import as_crispy_field
 from django.contrib.humanize.templatetags.humanize import intcomma, ordinal
 from django.templatetags.static import static
 from django.urls import reverse
-from jinja2 import Environment, Markup
+from jinja2 import Environment, Markup  # type: ignore [attr-defined]
 
 from library.utils import oxford_comma, round_trunc
 
 
-def environment(**options):
+def environment(**options: Any) -> Environment:
     env = Environment(**options, trim_blocks=True, lstrip_blocks=True)
 
     env.globals.update(
