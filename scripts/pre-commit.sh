50315298f789d4cbc90b383e50606fcd7b248f05
Benjamin Eskola <ben@eskola.uk>
2021-09-05 12:11:51 +0100

    Fix git hook to use poetry, new mypysettings, etc

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
-pipenv run find library/jinja2/ -name '*.html' -exec curlylint --rule 'indent: 2' {} +
-pipenv run flake8 library
-pipenv run pytest -q
-pipenv run mypy --show-error-codes -m library.models -m library.utils -m library.forms
+poetry run find library/jinja2/ -name '*.html' -exec curlylint --rule 'indent: 2' {} +
+poetry run flake8 library
+poetry run pytest -q | grep 'Total coverage:'
+poetry run mypy --show-error-codes --exclude '/(migrations|tests|management)/$' booklogger library
