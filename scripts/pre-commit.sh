f1916b9aa06c5fb4d9fd3a9852afeb73edbb3390
Benjamin Eskola <ben@eskola.uk>
2021-09-07 11:44:38 +0100

    Configure mypy entirely through the config file

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -3,4 +3,4 @@ set -e
 poetry run find library/jinja2/ -name '*.html' -exec curlylint --rule 'indent: 2' {} +
 poetry run flake8 library
 poetry run pytest -q | grep 'Total coverage:'
-poetry run mypy --show-error-codes --exclude '/(migrations|tests|management)/$' booklogger library
+poetry run mypy
