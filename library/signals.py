858d3444bf70ca88f00a54abc42822ac1b5ddae5
Benjamin Eskola <ben@eskola.uk>
2021-09-09 08:58:33 +0100

    Typecheck signals

--- library/signals.py
+++ library/signals.py
@@ -1,5 +1,16 @@
+from typing import Any, Type
+
 from django.utils import timezone
 
+from library.models.timestamped_model import TimestampedModel
 
-def update_timestamp_on_save(sender, instance, raw, using, update_fields, **kwargs):  # type: ignore [no-untyped-def]
+
+def update_timestamp_on_save(
+    sender: Type[TimestampedModel],
+    instance: TimestampedModel,
+    raw: bool,
+    using: str,
+    update_fields: dict[str, Any],
+    **kwargs: Any
+) -> None:
     instance.modified_date = timezone.now()
