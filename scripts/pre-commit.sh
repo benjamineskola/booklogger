5461d330e024926d583bd49d8b2a8292a4bbd454
Benjamin Eskola <ben@eskola.uk>
2021-10-25 12:07:21 +0100

    Switch to flakehell

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -e
 poetry run find library/jinja2/ -name '*.html' -exec curlylint --rule 'indent: 2' {} +
-poetry run flake8 library
+poetry run flakehell lint
 poetry run pytest -q | grep 'Total coverage:'
 poetry run mypy
