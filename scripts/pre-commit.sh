88e0d16b870ae4f03b13cf38dfd64f8bccec3b47
Benjamin Eskola <ben@eskola.uk>
2021-12-16 14:42:56 +0000

    Enable pylint linting

--- scripts/pre-commit.sh
+++ scripts/pre-commit.sh
@@ -2,5 +2,6 @@
 set -e
 poetry run find library/jinja2/ -name '*.html' -exec curlylint --rule 'indent: 2' {} +
 poetry run flake518
+poetry run pylint -- */
 poetry run pytest -q | grep 'Total coverage:'
 poetry run mypy
