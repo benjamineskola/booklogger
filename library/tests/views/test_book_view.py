55d50d6d56385ab3410d36170e1e4f10d863f72c
Benjamin Eskola <ben@eskola.uk>
2022-05-20 22:26:31 +0100

    Improve view tests by using standard client fixture

--- library/tests/views/test_book_view.py
+++ library/tests/views/test_book_view.py
@@ -1,90 +1,75 @@
 import pytest
 
 from library.models import Book, Tag
-from library.views.book import (
-    CurrentlyReadingView,
-    IndexView,
-    OwnedIndexView,
-    ReadView,
-    UnreadIndexView,
-)
 
 
 @pytest.mark.django_db
 class TestBook:
-    def test_currently_reading(self, get_response, book):
-        view = CurrentlyReadingView
-
-        resp = get_response(view)
+    def test_currently_reading(self, client, book):
+        resp = client.get("/")
         assert len(resp.context_data["object_list"]) == 0
 
         book.start_reading()
 
-        resp = get_response(view)
+        resp = client.get("/")
         assert len(resp.context_data["object_list"]) == 1
 
         book.finish_reading()
 
-        resp = get_response(view)
+        resp = client.get("/")
         assert len(resp.context_data["object_list"]) == 0
 
-    def test_read(self, get_response, book):
-        view = ReadView
-
-        resp = get_response(view)
+    def test_read(self, client, book):
+        resp = client.get("/books/read/")
         assert len(resp.context_data["object_list"]) == 0
 
         book.start_reading()
 
-        resp = get_response(view)
+        resp = client.get("/books/read/")
         assert len(resp.context_data["object_list"]) == 0
 
         book.finish_reading()
 
-        resp = get_response(view)
+        resp = client.get("/books/read/")
         assert len(resp.context_data["object_list"]) == 1
 
-    def test_toread(self, get_response, book, user):
-        view = UnreadIndexView
-
-        resp = get_response(view)
+    def test_toread(self, client, book, user):
+        resp = client.get("/books/toread/")
         assert len(resp.context_data["object_list"]) == 0
 
         book.owned_by = user
         book.save()
 
-        resp = get_response(view)
+        resp = client.get("/books/toread/")
         assert len(resp.context_data["object_list"]) == 1
         book.start_reading()
 
-        resp = get_response(view)
+        resp = client.get("/books/toread/")
         assert len(resp.context_data["object_list"]) == 0
 
         book.finish_reading()
 
-        resp = get_response(view)
+        resp = client.get("/books/toread/")
         assert len(resp.context_data["object_list"]) == 0
 
-    def test_owned(self, get_response, book, user):
-        view = OwnedIndexView
-
-        resp = get_response(view)
+    def test_owned(self, client, book, user):
+        resp = client.get("/books/owned/")
         assert len(resp.context_data["object_list"]) == 0
 
         book.owned_by = user
         book.save()
 
-        resp = get_response(view)
+        resp = client.get("/books/owned/")
         assert len(resp.context_data["object_list"]) == 1
 
     @pytest.mark.parametrize("format", ["EBOOK", "PAPERBACK", "HARDBACK", "WEB"])
     @pytest.mark.parametrize(
         "view_format", ["EBOOK", "PAPERBACK", "HARDBACK", "WEB", "PHYSICAL"]
     )
-    def test_format_filters(self, get_response, book_factory, format, view_format):
+    def test_format_filters(self, client, book_factory, format, view_format):
         book = book_factory(edition_format=Book.Format[format])
 
-        resp = get_response(IndexView, format=view_format)
+        resp = client.get(f"/books/{view_format.lower()}/")
 
         if view_format == format or (
             format in ["PAPERBACK", "HARDBACK"] and view_format == "PHYSICAL"
@@ -95,24 +80,24 @@ class TestBook:
 
     @pytest.mark.parametrize("tag", ["fiction", "non-fiction"])
     @pytest.mark.parametrize("view_tag", ["fiction", "non-fiction"])
-    def test_tag_filters(self, get_response, book_factory, tag, view_tag):
+    def test_tag_filters(self, client, book_factory, tag, view_tag):
         book = book_factory(tags=[tag])
         [Tag(name=name).save() for name in ["fiction", "non-fiction"]]
 
-        resp = get_response(IndexView, get={"tags": view_tag})
+        resp = client.get("/books/", {"tags": view_tag})
         if tag == view_tag:
             assert book in resp.context_data["object_list"]
         else:
             assert book not in resp.context_data["object_list"]
 
-    def test_sort_order(self, get_response, book_factory):
+    def test_sort_order(self, client, book_factory):
         book1 = book_factory(title="AAAAA", first_published=1999)
         book2 = book_factory(title="BBBBB", first_published=2000)
 
-        resp = get_response(IndexView, sort_by="title")
+        resp = client.get("/books/", {"sort_by": "title"})
         assert resp.context_data["object_list"][0] == book1
         assert resp.context_data["object_list"][1] == book2
 
-        resp = get_response(IndexView, sort_by="first_published")
+        resp = client.get("/books/", {"sort_by": "first_published"})
         assert resp.context_data["object_list"][0] == book2
         assert resp.context_data["object_list"][1] == book1
--- library/tests/views/test_report_view.py
+++ library/tests/views/test_report_view.py
@@ -5,31 +5,35 @@ from library.views.report import IndexView
 
 @pytest.mark.django_db
 class TestReportView:
-    def test_report_index(self, get_response, book_factory, user):
+    def test_report_admin_only(self, client):
+        assert client.get("/report/").status_code == 302
+        assert client.get("/report/1/").status_code == 302
+
+    def test_report_index(self, admin_client, book_factory, user):
         book_factory(isbn="", owned_by=user)
 
-        resp = get_response(IndexView)
+        resp = admin_client.get("/report/")
         assert len(resp.context_data["object_list"]) == 0
         assert resp.context_data["page_title"] == "Reports"
 
-    def test_report_page(self, get_response, book_factory, user):
+    def test_report_page(self, admin_client, book_factory, user):
         book_factory(isbn="", owned_by=user)
 
-        resp = get_response(IndexView, page="1")
+        resp = admin_client.get("/report/1/")
         assert len(resp.context_data["object_list"]) == 1
 
     @pytest.mark.parametrize("page", range(1, len(IndexView().categories)))
-    def test_report_all_pages(self, page, get_response, book_factory, user):
+    def test_report_all_pages(self, page, admin_client, book_factory, user):
         book_factory(owned_by=user, tags=["non-fiction", "history"])
-        resp = get_response(IndexView, page=str(page))
+        resp = admin_client.get(f"/report/{page}/")
         assert resp.context_data["page_title"] != "Reports"
 
-    def test_report_ordering(self, get_response, book_factory, user):
+    def test_report_ordering(self, admin_client, book_factory, user):
         book1 = book_factory(title="Book A", page_count=200, isbn="", owned_by=user)
         book2 = book_factory(title="Book B", page_count=100, isbn="", owned_by=user)
 
-        resp = get_response(IndexView, get={"order_by": "title"}, page="1")
+        resp = admin_client.get("/report/1/", {"order_by": "title"})
         assert list(resp.context_data["object_list"]) == [book1, book2]
 
-        resp = get_response(IndexView, get={"order_by": "page_count"}, page="1")
+        resp = admin_client.get("/report/1/", {"order_by": "page_count"})
         assert list(resp.context_data["object_list"]) == [book2, book1]
