94296d7969b092166283f87eeb783a8e73c0a186
Benjamin Eskola <ben@eskola.uk>
2022-05-21 13:10:13 +0100

    Add test for reading list view

---
+++ library/tests/views/test_reading_list_view.py
@@ -0,0 +1,24 @@
+import pytest
+
+from library.models import ReadingList
+
+
+@pytest.mark.django_db
+class TestReadingList:
+    def test_index(self, book_factory, client):
+        reading_list = ReadingList()
+        reading_list.save()
+
+        resp = client.get("/lists/")
+        assert reading_list in resp.context_data["object_list"]
+
+    def test_detail(self, book_factory, client):
+        reading_list = ReadingList()
+        reading_list.save()
+        book1 = book_factory()
+        book2 = book_factory()
+        reading_list.books.add(book1)
+
+        resp = client.get(f"/list/{reading_list.id}/")
+        assert book1 in resp.context_data["object"].books.all()
+        assert book2 not in resp.context_data["object"].books.all()
