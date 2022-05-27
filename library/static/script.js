ac4be1844620692d5e7e0b7645ac7e625aef5cb2
Benjamin Eskola <ben@eskola.uk>
2022-05-27 17:40:55 +0100

    Use standard for js

--- library/static/script.js
+++ library/static/script.js
@@ -1,55 +1,55 @@
 $(document).ready(function () {
-  $("a.remove-tag").on("click", remove_tag);
-  $("form.add-tag").on("submit", add_tag);
-  $("form.update-progress").on("submit", update_progress);
-  $("span.rating-star").on("click", rate_book);
+  $('a.remove-tag').on('click', remove_tag)
+  $('form.add-tag').on('submit', add_tag)
+  $('form.update-progress').on('submit', update_progress)
+  $('span.rating-star').on('click', rate_book)
 
-  $("div.stats-for-year").each(function (i, e) {
-    return load_stats_for_year(e);
-  });
-});
+  $('div.stats-for-year').each(function (i, e) {
+    return load_stats_for_year(e)
+  })
+})
 
-function load_next_page(year, url) {
-  var placeholder = $(".loader");
+function load_next_page (year, url) {
+  const placeholder = $('.loader')
   $.ajax({
-    type: "GET",
-    url: url,
+    type: 'GET',
+    url,
     success: function (data) {
-      var body = $(data).find(".book.card").parent().parent().parent();
-      body.insertAfter(placeholder.parent());
-      placeholder.remove();
+      const body = $(data).find('.book.card').parent().parent().parent()
+      body.insertAfter(placeholder.parent())
+      placeholder.remove()
 
-      body.find("a.remove-tag").on("click", remove_tag);
-      body.find("form.add-tag").on("submit", add_tag);
-      body.find("form.update-progress").on("submit", update_progress);
-      body.find("span.rating-star").on("click", rate_book);
+      body.find('a.remove-tag').on('click', remove_tag)
+      body.find('form.add-tag').on('submit', add_tag)
+      body.find('form.update-progress').on('submit', update_progress)
+      body.find('span.rating-star').on('click', rate_book)
 
-      if (window.location.hash.split("-").pop() == year) {
-        $("html, body").animate(
+      if (window.location.hash.split('-').pop() == year) {
+        $('html, body').animate(
           {
-            scrollTop: body.offset().top,
+            scrollTop: body.offset().top
           },
           10
-        );
+        )
       }
     },
     error: function () {
-      placeholder.find(".spinner-grow").addClass("d-none");
-      placeholder.find(".alert-danger").removeClass("d-none");
-    },
-  });
+      placeholder.find('.spinner-grow').addClass('d-none')
+      placeholder.find('.alert-danger').removeClass('d-none')
+    }
+  })
 }
 
-function load_stats_for_year(e) {
-  var div = $(e);
-  var year = div.data("year");
+function load_stats_for_year (e) {
+  const div = $(e)
+  const year = div.data('year')
 
   $.ajax({
-    type: "GET",
+    type: 'GET',
     url: `/stats/${year}`,
     success: function (data) {
-      $(".spinner-grow").show();
-      div.html(data);
+      $('.spinner-grow').show()
+      div.html(data)
     },
     error: function (data) {
       div.html(`
@@ -60,158 +60,152 @@ function load_stats_for_year(e) {
               <p><span class="btn btn-outline-danger">Retry?</span></p>
             </div>
           </div>
-`);
-      $(`#error-${year} .btn`).on("click", function () {
+`)
+      $(`#error-${year} .btn`).on('click', function () {
         div.html(`
           <div id="loading-stats-${year}" class="spinner-grow mb-4" role="status">
             <span class="sr-only">Loading...</span>
           </div>
-        `);
-        return load_stats_for_year(e);
-      });
-    },
-  });
+        `)
+        return load_stats_for_year(e)
+      })
+    }
+  })
 }
 
-function add_tag(event) {
-  event.preventDefault();
-  var form = $(this);
-  var url = form.attr("action");
+function add_tag (event) {
+  event.preventDefault()
+  const form = $(this)
+  const url = form.attr('action')
 
   $.ajax({
-    type: "POST",
-    url: url,
+    type: 'POST',
+    url,
     data: form.serialize(),
-    dataType: "json",
+    dataType: 'json',
     success: function (data) {
-      var tags_field = $(form.data("tags"));
-      var input_field = form.find('input[name="tags"]');
-      for (var i in data.tags) {
-        new_tag = data.tags[i];
+      const tags_field = $(form.data('tags'))
+      const input_field = form.find('input[name="tags"]')
+      for (const i in data.tags) {
+        new_tag = data.tags[i]
         tags_field.prepend(
           `<span class="badge badge-secondary"><a href="/tag/${new_tag}">${new_tag}</a></span> `
-        );
+        )
       }
-      input_field.val("");
-      form.collapse("hide");
-    },
-  });
+      input_field.val('')
+      form.collapse('hide')
+    }
+  })
 }
 
-function rate_book() {
-  console.log(this);
-  var value = $(this).data("rating");
-  var ratings = $(this).parent();
-  var book = ratings.data("book");
-  var old_rating = ratings.data("rating");
+function rate_book () {
+  console.log(this)
+  let value = $(this).data('rating')
+  const ratings = $(this).parent()
+  const book = ratings.data('book')
+  const old_rating = ratings.data('rating')
 
   if (old_rating == value) {
-    value = value - 0.5;
+    value = value - 0.5
   } else if (old_rating == value - 0.5) {
-    value = 0;
+    value = 0
   }
 
   $.ajax({
-    type: "POST",
+    type: 'POST',
     url: `/book/${book}/rate/`,
     data: { rating: Number(value) },
     beforeSend: function (xhr, settings) {
-      xhr.setRequestHeader(
-        "X-CSRFToken",
-        $("[name=csrfmiddlewaretoken]").val()
-      );
+      xhr.setRequestHeader('X-CSRFToken', $('[name=csrfmiddlewaretoken]').val())
     },
     success: function (data) {
-      ratings.data("rating", value);
+      ratings.data('rating', value)
       ratings.children().each(function (i, star) {
         if (value >= i + 1) {
-          $(star).text("★");
+          $(star).text('★')
         } else if (value - i == 0.5) {
-          $(star).text("½");
+          $(star).text('½')
         } else {
-          $(star).text("☆");
+          $(star).text('☆')
         }
-      });
-    },
-  });
+      })
+    }
+  })
 }
 
-function remove_tag() {
-  var tag = $(this).data("tag");
-  var label = $(this).parent();
-  var book = label.parent().data("book");
+function remove_tag () {
+  const tag = $(this).data('tag')
+  const label = $(this).parent()
+  const book = label.parent().data('book')
   $.ajax({
-    type: "POST",
+    type: 'POST',
     url: `/book/${book}/remove_tags/`,
     data: { tags: tag },
     beforeSend: function (xhr, settings) {
-      xhr.setRequestHeader(
-        "X-CSRFToken",
-        $("[name=csrfmiddlewaretoken]").val()
-      );
-      return confirm(`Remove tag ${tag}?`);
+      xhr.setRequestHeader('X-CSRFToken', $('[name=csrfmiddlewaretoken]').val())
+      return confirm(`Remove tag ${tag}?`)
     },
     success: function (data) {
-      label.remove();
-    },
-  });
+      label.remove()
+    }
+  })
 }
 
-function update_progress(event) {
-  event.preventDefault();
-  var form = $(this);
-  var url = form.attr("action");
-  var book = form.data("book");
+function update_progress (event) {
+  event.preventDefault()
+  const form = $(this)
+  const url = form.attr('action')
+  const book = form.data('book')
 
   $.ajax({
-    type: "POST",
-    url: url,
+    type: 'POST',
+    url,
     data: form.serialize(),
-    dataType: "json",
+    dataType: 'json',
     success: function (data) {
-      var progress_bar = $(`#book-${book} .progress-bar`);
-      progress_bar.css("width", `${data["percentage"]}%`);
+      const progress_bar = $(`#book-${book} .progress-bar`)
+      progress_bar.css('width', `${data.percentage}%`)
 
-      var progress_text = $(`#book-${book} .card-footer .progress-date`);
+      var progress_text = $(`#book-${book} .card-footer .progress-date`)
       if (progress_text.length == 0) {
-        var progress_text = $(`#book-${book} .card-footer .read-dates`);
-        progress_text.text(`${progress_text.text()}; ${data["progress_text"]}`);
+        var progress_text = $(`#book-${book} .card-footer .read-dates`)
+        progress_text.text(`${progress_text.text()}; ${data.progress_text}`)
       } else {
-        progress_text.text(data["progress_text"]);
+        progress_text.text(data.progress_text)
       }
 
-      form.find('input[name="value"]').val("");
-    },
-  });
+      form.find('input[name="value"]').val('')
+    }
+  })
 }
 
-function update_scroll_position() {
-  $("h2").each(function () {
-    const height = $(window).height();
-    var top = window.pageYOffset;
-    var distance = top - $(this).offset().top;
-    var hash = $(this).attr("href");
+function update_scroll_position () {
+  $('h2').each(function () {
+    const height = $(window).height()
+    const top = window.pageYOffset
+    const distance = top - $(this).offset().top
+    const hash = $(this).attr('href')
     if (Math.abs(distance) < height * 0.75) {
       if (window.location.hash != hash) {
         if (history.pushState) {
-          history.pushState(null, null, hash);
+          history.pushState(null, null, hash)
         } else {
-          window.location.hash = hash;
+          window.location.hash = hash
         }
       }
     } else if (0 - distance > height && hash == window.location.hash) {
-      var year = hash.split("-").pop();
-      if (year == "sometime") {
-        var new_year = 2004;
+      const year = hash.split('-').pop()
+      if (year == 'sometime') {
+        var new_year = 2004
       } else {
-        var new_year = parseInt(year) + 1;
+        var new_year = parseInt(year) + 1
       }
-      var new_hash = `#read-${new_year}`;
+      const new_hash = `#read-${new_year}`
       if (history.pushState) {
-        history.pushState(null, null, new_hash);
+        history.pushState(null, null, new_hash)
       } else {
-        window.location.hash = new_hash;
+        window.location.hash = new_hash
       }
     }
-  });
+  })
 }
