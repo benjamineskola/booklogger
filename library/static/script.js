99405ed694ef2ec43f2d4a9ad4569f42f293f18a
Benjamin Eskola <ben@eskola.uk>
2022-05-27 17:59:07 +0100

    Fix StandardJS warnings

--- library/static/script.js
+++ library/static/script.js
@@ -1,14 +1,17 @@
+/* globals $, confirm, history */
+
 $(document).ready(function () {
-  $('a.remove-tag').on('click', remove_tag)
-  $('form.add-tag').on('submit', add_tag)
-  $('form.update-progress').on('submit', update_progress)
-  $('span.rating-star').on('click', rate_book)
+  $('a.remove-tag').on('click', removeTag)
+  $('form.add-tag').on('submit', addTag)
+  $('form.update-progress').on('submit', updateProgress)
+  $('span.rating-star').on('click', rateBook)
 
   $('div.stats-for-year').each(function (i, e) {
-    return load_stats_for_year(e)
+    return loadStatsForYear(e)
   })
 })
 
+/* eslint-disable-next-line camelcase, no-unused-vars */
 function load_next_page (year, url) {
   const placeholder = $('.loader')
   $.ajax({
@@ -19,12 +22,12 @@ function load_next_page (year, url) {
       body.insertAfter(placeholder.parent())
       placeholder.remove()
 
-      body.find('a.remove-tag').on('click', remove_tag)
-      body.find('form.add-tag').on('submit', add_tag)
-      body.find('form.update-progress').on('submit', update_progress)
-      body.find('span.rating-star').on('click', rate_book)
+      body.find('a.remove-tag').on('click', removeTag)
+      body.find('form.add-tag').on('submit', addTag)
+      body.find('form.update-progress').on('submit', updateProgress)
+      body.find('span.rating-star').on('click', rateBook)
 
-      if (window.location.hash.split('-').pop() == year) {
+      if (window.location.hash.split('-').pop() === year) {
         $('html, body').animate(
           {
             scrollTop: body.offset().top
@@ -40,7 +43,7 @@ function load_next_page (year, url) {
   })
 }
 
-function load_stats_for_year (e) {
+function loadStatsForYear (e) {
   const div = $(e)
   const year = div.data('year')
 
@@ -67,13 +70,13 @@ function load_stats_for_year (e) {
             <span class="sr-only">Loading...</span>
           </div>
         `)
-        return load_stats_for_year(e)
+        return loadStatsForYear(e)
       })
     }
   })
 }
 
-function add_tag (event) {
+function addTag (event) {
   event.preventDefault()
   const form = $(this)
   const url = form.attr('action')
@@ -84,30 +87,30 @@ function add_tag (event) {
     data: form.serialize(),
     dataType: 'json',
     success: function (data) {
-      const tags_field = $(form.data('tags'))
-      const input_field = form.find('input[name="tags"]')
+      const tagsField = $(form.data('tags'))
+      const inputField = form.find('input[name="tags"]')
       for (const i in data.tags) {
-        new_tag = data.tags[i]
-        tags_field.prepend(
-          `<span class="badge badge-secondary"><a href="/tag/${new_tag}">${new_tag}</a></span> `
+        const newTag = data.tags[i]
+        tagsField.prepend(
+          `<span class="badge badge-secondary"><a href="/tag/${newTag}">${newTag}</a></span> `
         )
       }
-      input_field.val('')
+      inputField.val('')
       form.collapse('hide')
     }
   })
 }
 
-function rate_book () {
+function rateBook () {
   console.log(this)
   let value = $(this).data('rating')
   const ratings = $(this).parent()
   const book = ratings.data('book')
-  const old_rating = ratings.data('rating')
+  const oldRating = ratings.data('rating')
 
-  if (old_rating == value) {
+  if (oldRating === value) {
     value = value - 0.5
-  } else if (old_rating == value - 0.5) {
+  } else if (oldRating === value - 0.5) {
     value = 0
   }
 
@@ -123,7 +126,7 @@ function rate_book () {
       ratings.children().each(function (i, star) {
         if (value >= i + 1) {
           $(star).text('★')
-        } else if (value - i == 0.5) {
+        } else if (value - i === 0.5) {
           $(star).text('½')
         } else {
           $(star).text('☆')
@@ -133,13 +136,13 @@ function rate_book () {
   })
 }
 
-function remove_tag () {
+function removeTag () {
   const tag = $(this).data('tag')
   const label = $(this).parent()
   const book = label.parent().data('book')
   $.ajax({
     type: 'POST',
-    url: `/book/${book}/remove_tags/`,
+    url: `/book/${book}/remove_tag/`,
     data: { tags: tag },
     beforeSend: function (xhr, settings) {
       xhr.setRequestHeader('X-CSRFToken', $('[name=csrfmiddlewaretoken]').val())
@@ -151,7 +154,7 @@ function remove_tag () {
   })
 }
 
-function update_progress (event) {
+function updateProgress (event) {
   event.preventDefault()
   const form = $(this)
   const url = form.attr('action')
@@ -163,15 +166,15 @@ function update_progress (event) {
     data: form.serialize(),
     dataType: 'json',
     success: function (data) {
-      const progress_bar = $(`#book-${book} .progress-bar`)
-      progress_bar.css('width', `${data.percentage}%`)
+      const progressBar = $(`#book-${book} .progress-bar`)
+      progressBar.css('width', `${data.percentage}%`)
 
-      var progress_text = $(`#book-${book} .card-footer .progress-date`)
-      if (progress_text.length == 0) {
-        var progress_text = $(`#book-${book} .card-footer .read-dates`)
-        progress_text.text(`${progress_text.text()}; ${data.progress_text}`)
+      let progressText = $(`#book-${book} .card-footer .progress-date`)
+      if (progressText.length === 0) {
+        progressText = $(`#book-${book} .card-footer .read-dates`)
+        progressText.text(`${progressText.text()}; ${data.progress_text}`)
       } else {
-        progress_text.text(data.progress_text)
+        progressText.text(data.progress_text)
       }
 
       form.find('input[name="value"]').val('')
@@ -179,6 +182,7 @@ function update_progress (event) {
   })
 }
 
+/* eslint-disable-next-line camelcase, no-unused-vars */
 function update_scroll_position () {
   $('h2').each(function () {
     const height = $(window).height()
@@ -186,25 +190,26 @@ function update_scroll_position () {
     const distance = top - $(this).offset().top
     const hash = $(this).attr('href')
     if (Math.abs(distance) < height * 0.75) {
-      if (window.location.hash != hash) {
+      if (window.location.hash !== hash) {
         if (history.pushState) {
           history.pushState(null, null, hash)
         } else {
           window.location.hash = hash
         }
       }
-    } else if (0 - distance > height && hash == window.location.hash) {
+    } else if (0 - distance > height && hash === window.location.hash) {
       const year = hash.split('-').pop()
-      if (year == 'sometime') {
-        var new_year = 2004
+      let newYear
+      if (year === 'sometime') {
+        newYear = 2004
       } else {
-        var new_year = parseInt(year) + 1
+        newYear = parseInt(year) + 1
       }
-      const new_hash = `#read-${new_year}`
+      const newHash = `#read-${newYear}`
       if (history.pushState) {
-        history.pushState(null, null, new_hash)
+        history.pushState(null, null, newHash)
       } else {
-        window.location.hash = new_hash
+        window.location.hash = newHash
       }
     }
   })
