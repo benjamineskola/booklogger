6fd9b5ba8054f8fd9d9e6bc5bce23b22b677bdad
Benjamin Eskola <ben@eskola.uk>
2022-10-29 11:08:10 +0100

    Update javascript style

--- library/static/book-form.js
+++ library/static/book-form.js
@@ -1,13 +1,13 @@
 /* globals $ */
 
 $(document).ready(function () {
-  configureSelectFields()
+  configureSelectFields();
 
   // @ts-ignore
   if (document.formTags !== null) {
     // @ts-ignore
-    $('#id_tags').val(document.formTags)
-    $('#id_tags').trigger('change')
+    $('#id_tags').val(document.formTags);
+    $('#id_tags').trigger('change');
   }
 
   for (const fieldName of [
@@ -15,7 +15,7 @@ $(document).ready(function () {
     'alienated_date',
     'ebook_acquired_date'
   ]) {
-    configureDateFields(fieldName)
+    configureDateFields(fieldName);
   }
 
   $('#card-bookauthor_set .card-footer a').on(
@@ -23,194 +23,202 @@ $(document).ready(function () {
     // @ts-ignore
     { template: document.bookauthor_formset_template },
     addAuthorField
-  )
+  );
   $('#card-readinglistentry_set .card-footer a').on(
     'click',
     // @ts-ignore
     { template: document.listentry_formset_template },
     addListEntryField
-  )
+  );
 
   $('#card-bookauthor_set, #card-logentry_set, #card-readinglistentry_set')
     .find('.form-check-input')
     .on('click', function (event) {
-      $(this).closest('.form-inline').hide(1000)
-    })
+      $(this)
+        .closest('.form-inline')
+        .hide(1000);
+    });
 
   for (const elementId of ['acquired', 'alienated', 'ebook_acquired']) {
-    addSetDateTodayButton(elementId)
+    addSetDateTodayButton(elementId);
   }
 
-  isbnFieldPasteFilter('#id_isbn, #id_ebook_isbn')
-})
+  isbnFieldPasteFilter('#id_isbn, #id_ebook_isbn');
+});
 
 /** @param {JQueryEventObject} event */
 function addAuthorField (event) {
-  event.preventDefault()
+  event.preventDefault();
 
-  const parent = $('#formset-bookauthor_set')
-  const totalForms = $('input[name="bookauthor_set-TOTAL_FORMS"]')
-  const index = parseInt(String(totalForms.val()))
+  const parent = $('#formset-bookauthor_set');
+  const totalForms = $('input[name="bookauthor_set-TOTAL_FORMS"]');
+  const index = parseInt(String(totalForms.val()));
 
-  const inlineForm = $('<div class="form-inline"></div>')
+  const inlineForm = $('<div class="form-inline"></div>');
   const selectField = $(event.data.template.replace(/__prefix__/g, index)).find(
     'select'
-  )
+  );
 
-  const authorFormGroup = $('<div class="form-group"></div>')
+  const authorFormGroup = $('<div class="form-group"></div>');
   authorFormGroup.append(
     `<label for="id_bookauthor_set-${index}-author" class="mr-2 requiredField">Author*</label>`
-  )
-  const selectDiv = $('<div class="mr-2 mt-2"></div>')
-  selectDiv.append(selectField)
-  authorFormGroup.append(selectDiv)
-  inlineForm.append(authorFormGroup)
+  );
+  const selectDiv = $('<div class="mr-2 mt-2"></div>');
+  selectDiv.append(selectField);
+  authorFormGroup.append(selectDiv);
+  inlineForm.append(authorFormGroup);
 
   inlineForm.append(
     $(
       `<div class="form-group"><label for="bookauthor_set-${index}-role" class="mr-2">Role</label><div class="mr-2 mt-2"><input type="text" name="bookauthor_set-${index}-role" maxlength="255" class="textinput textInput form-control" id="id_bookauthor_set-${index}-role"></div></div>`
     )
-  )
+  );
   inlineForm.append(
     $(
       `<div class="form-group"><label for="bookauthor_set-${index}-order" class="mr-2">Order</label><div class="mr-2 mt-2"><input type="number" name="bookauthor_set-${index}-order" maxlength="255" class="textinput textInput form-control" id="id_bookauthor_set-${index}-role"></div></div>`
     )
-  )
+  );
   inlineForm.append(
     $(
       `<div class="form-group"><div class="mr-2 mt-2"><div class="form-check"><input type="checkbox" name="bookauthor_set-${index}-DELETE" class="checkboxinput form-check-input" id="id_bookauthor_set-${index}-DELETE"><label for="id_bookauthor_set-${index}-DELETE" class="form-check-label">Delete </label></div></div></div>`
     )
-  )
+  );
 
-  parent.append(inlineForm)
-  totalForms.val(index + 1)
+  parent.append(inlineForm);
+  totalForms.val(index + 1);
 
   // @ts-ignore
-  selectField.select2({ theme: 'bootstrap', tags: 'true' })
+  selectField.select2({ theme: 'bootstrap', tags: 'true' });
   inlineForm.find('.form-check-input').on('click', function (event) {
-    $(this).closest('.form-inline').hide(1000)
-  })
+    $(this)
+      .closest('.form-inline')
+      .hide(1000);
+  });
 }
 
 /** @param {JQueryEventObject} event */
 function addListEntryField (event) {
-  event.preventDefault()
+  event.preventDefault();
 
-  const parent = $('#formset-readinglistentry_set')
-  const totalForms = $('input[name="readinglistentry_set-TOTAL_FORMS"]')
-  const index = parseInt(String(totalForms.val()))
+  const parent = $('#formset-readinglistentry_set');
+  const totalForms = $('input[name="readinglistentry_set-TOTAL_FORMS"]');
+  const index = parseInt(String(totalForms.val()));
 
-  const inlineForm = $('<div class="form-inline"></div>')
+  const inlineForm = $('<div class="form-inline"></div>');
   const selectField = $(event.data.template.replace(/__prefix__/g, index)).find(
     'select'
-  )
+  );
 
-  const authorFormGroup = $('<div class="form-group"></div>')
+  const authorFormGroup = $('<div class="form-group"></div>');
   authorFormGroup.append(
     `<label for="id_readinglistentry_set-${index}-reading_list}" class="mr-2 requiredField">Reading list*</label>`
-  )
-  const selectDiv = $('<div class="mr-2 mt-2"></div>')
-  selectDiv.append(selectField)
-  authorFormGroup.append(selectDiv)
-  inlineForm.append(authorFormGroup)
+  );
+  const selectDiv = $('<div class="mr-2 mt-2"></div>');
+  selectDiv.append(selectField);
+  authorFormGroup.append(selectDiv);
+  inlineForm.append(authorFormGroup);
 
   inlineForm.append(
     $(
       `<div class="form-group"><label for="readinglistentry_set-${index}-order" class="mr-2">Order</label><div class="mr-2 mt-2"><input type="number" name="readinglistentry_set-${index}-order" maxlength="255" class="textinput textInput form-control" id="id_readinglistentry_set-${index}-role"></div></div>`
     )
-  )
+  );
   inlineForm.append(
     $(
       `<div class="form-group"><div class="mr-2 mt-2"><div class="form-check"><input type="checkbox" name="readinglistentry_set-${index}-DELETE" class="checkboxinput form-check-input" id="id_readinglistentry_set-${index}-DELETE"><label for="id_readinglistentry_set-${index}-DELETE" class="form-check-label">Delete </label></div></div></div>`
     )
-  )
+  );
 
-  parent.append(inlineForm)
-  totalForms.val(index + 1)
+  parent.append(inlineForm);
+  totalForms.val(index + 1);
 
-  selectField.select2({ theme: 'bootstrap' })
+  selectField.select2({ theme: 'bootstrap' });
   inlineForm.find('.form-check-input').on('click', function () {
-    $(this).closest('.form-inline').hide(1000)
-  })
+    $(this)
+      .closest('.form-inline')
+      .hide(1000);
+  });
 }
 
 /** @param {JQueryEventObject} event */
 function setDateToday (event) {
-  event.preventDefault()
-  const elementId = event.data.elementId
+  event.preventDefault();
+  const elementId = event.data.elementId;
 
-  const date = new Date()
+  const date = new Date();
 
-  $('#id_' + elementId + '_date_month').val(date.getMonth() + 1)
-  $('#id_' + elementId + '_date_month').trigger('change')
+  $('#id_' + elementId + '_date_month').val(date.getMonth() + 1);
+  $('#id_' + elementId + '_date_month').trigger('change');
 
-  $('#id_' + elementId + '_date_year').val(date.getFullYear())
-  $('#id_' + elementId + '_date_year').trigger('change')
+  $('#id_' + elementId + '_date_year').val(date.getFullYear());
+  $('#id_' + elementId + '_date_year').trigger('change');
 
-  $('#id_' + elementId + '_date_day').val(date.getDate())
-  $('#id_' + elementId + '_date_day').trigger('change')
+  $('#id_' + elementId + '_date_day').val(date.getDate());
+  $('#id_' + elementId + '_date_day').trigger('change');
 }
 
 function configureSelectFields () {
-  $('select').select2({ theme: 'bootstrap' })
+  $('select').select2({ theme: 'bootstrap' });
   $(
     '#id_first_author, #id_publisher, #id_series, #formset-bookauthor_set select'
   ).select2({
     theme: 'bootstrap',
     tags: true
-  })
+  });
   $('#id_tags').select2({
     theme: 'bootstrap',
     tags: true,
     tokenSeparators: [',']
-  })
+  });
 }
 
 /** @param {string} fieldName */
 function configureDateFields (fieldName) {
-  $(`#id_${fieldName}_day`).select2({ theme: 'bootstrap', width: '5em' })
-  $(`#id_${fieldName}_month`).select2({ theme: 'bootstrap', width: '9em' })
+  $(`#id_${fieldName}_day`).select2({ theme: 'bootstrap', width: '5em' });
+  $(`#id_${fieldName}_month`).select2({ theme: 'bootstrap', width: '9em' });
   $(`#id_${fieldName}_year`).select2({
     theme: 'bootstrap',
     tags: true,
     width: '6em'
-  })
+  });
 
-  $(`#id_${fieldName}_month`).parent().addClass('form-row')
+  $(`#id_${fieldName}_month`)
+    .parent()
+    .addClass('form-row');
   $(`#id_${fieldName}_month`)
     .parent()
     .find('span')
     .each(function () {
-      $(this).addClass('mr-2')
-    })
+      $(this).addClass('mr-2');
+    });
 }
 
 /** @param {string} elementId */
 function addSetDateTodayButton (elementId) {
   $(`#div_id_${elementId}_date`).append(
     `<a href="#" id="${elementId}_date_set_today">Set to today</a>`
-  )
+  );
 
   $(`#${elementId}_date_set_today`).on(
     'click',
     null,
     { elementId },
     setDateToday
-  )
+  );
 }
 
 /** @param {string} selector, @this {Element} */
 function isbnFieldPasteFilter (selector) {
   $(selector).on('paste', function (ev) {
-    ev.preventDefault()
+    ev.preventDefault();
     // @ts-ignore
-    const pastedText = ev.originalEvent.clipboardData.getData('text/plain')
-    const elem = $(this)
-    elem.val(pastedText.replace(/\D/g, ''))
-  })
+    const pastedText = ev.originalEvent.clipboardData.getData('text/plain');
+    const elem = $(this);
+    elem.val(pastedText.replace(/\D/g, ''));
+  });
   $(selector).on('input', function (ev) {
-    ev.preventDefault()
-    const elem = $(this)
-    elem.val(String(elem.val()).replace(/\D/g, ''))
-  })
+    ev.preventDefault();
+    const elem = $(this);
+    elem.val(String(elem.val()).replace(/\D/g, ''));
+  });
 }
--- library/static/script.js
+++ library/static/script.js
@@ -1,15 +1,15 @@
 /* globals $, confirm, history */
 
 $(document).ready(function () {
-  $('a.remove-tag').on('click', removeTag)
-  $('form.add-tag').on('submit', addTag)
-  $('form.update-progress').on('submit', updateProgress)
-  $('span.rating-star').on('click', rateBook)
+  $('a.remove-tag').on('click', removeTag);
+  $('form.add-tag').on('submit', addTag);
+  $('form.update-progress').on('submit', updateProgress);
+  $('span.rating-star').on('click', rateBook);
 
   $('div.stats-for-year').each(function (i, e) {
-    return loadStatsForYear(e)
-  })
-})
+    return loadStatsForYear(e);
+  });
+});
 
 /**
  * @param {string} year
@@ -17,50 +17,52 @@ $(document).ready(function () {
  */
 // eslint-disable-next-line no-unused-vars
 function loadNextPage (year, url) {
-  const placeholder = $('.loader')
+  const placeholder = $('.loader');
   $.ajax({
     type: 'GET',
     url,
     success: function (data) {
-      const body = $(data).find('.book.card').closest('body')
-      body.insertAfter(placeholder.parent())
-      placeholder.remove()
+      const body = $(data)
+        .find('.book.card')
+        .closest('body');
+      body.insertAfter(placeholder.parent());
+      placeholder.remove();
 
-      body.find('a.remove-tag').on('click', removeTag)
-      body.find('form.add-tag').on('submit', addTag)
-      body.find('form.update-progress').on('submit', updateProgress)
-      body.find('span.rating-star').on('click', rateBook)
+      body.find('a.remove-tag').on('click', removeTag);
+      body.find('form.add-tag').on('submit', addTag);
+      body.find('form.update-progress').on('submit', updateProgress);
+      body.find('span.rating-star').on('click', rateBook);
 
       if (window.location.hash.split('-').pop() === year) {
-        const offset = body.offset()
+        const offset = body.offset();
         if (typeof offset !== 'undefined') {
           $('html, body').animate(
             {
               scrollTop: offset.top
             },
             10
-          )
+          );
         }
       }
     },
     error: function () {
-      placeholder.find('.spinner-grow').addClass('d-none')
-      placeholder.find('.alert-danger').removeClass('d-none')
+      placeholder.find('.spinner-grow').addClass('d-none');
+      placeholder.find('.alert-danger').removeClass('d-none');
     }
-  })
+  });
 }
 
 /** @param {Element} element */
 function loadStatsForYear (element) {
-  const div = $(element)
-  const year = div.data('year')
+  const div = $(element);
+  const year = div.data('year');
 
   $.ajax({
     type: 'GET',
     url: `/stats/${year}`,
     success: function (data) {
-      $('.spinner-grow').show()
-      div.html(data)
+      $('.spinner-grow').show();
+      div.html(data);
     },
     error: function () {
       div.html(`
@@ -71,24 +73,24 @@ function loadStatsForYear (element) {
               <p><span class="btn btn-outline-danger">Retry?</span></p>
             </div>
           </div>
-`)
+`);
       $(`#error-${year} .btn`).on('click', function () {
         div.html(`
           <div id="loading-stats-${year}" class="spinner-grow mb-4" role="status">
             <span class="sr-only">Loading...</span>
           </div>
-        `)
-        return loadStatsForYear(element)
-      })
+        `);
+        return loadStatsForYear(element);
+      });
     }
-  })
+  });
 }
 
 /** @this Element, @param {Event} event */
 function addTag (event) {
-  event.preventDefault()
-  const form = $(this)
-  const url = form.attr('action')
+  event.preventDefault();
+  const form = $(this);
+  const url = form.attr('action');
 
   $.ajax({
     type: 'POST',
@@ -96,32 +98,32 @@ function addTag (event) {
     data: form.serialize(),
     dataType: 'json',
     success: function (data) {
-      const tagsField = $(form.data('tags'))
-      const inputField = form.find('input[name="tags"]')
+      const tagsField = $(form.data('tags'));
+      const inputField = form.find('input[name="tags"]');
       for (const i in data.tags) {
-        const newTag = data.tags[i]
+        const newTag = data.tags[i];
         tagsField.prepend(
           `<span class="badge badge-secondary"><a href="/tag/${newTag}">${newTag}</a></span> `
-        )
+        );
       }
-      inputField.val('')
+      inputField.val('');
       // @ts-ignore
-      form.collapse('hide')
+      form.collapse('hide');
     }
-  })
+  });
 }
 
 /** @this Element  */
 function rateBook () {
-  let value = $(this).data('rating')
-  const ratings = $(this).parent()
-  const book = ratings.data('book')
-  const oldRating = ratings.data('rating')
+  let value = $(this).data('rating');
+  const ratings = $(this).parent();
+  const book = ratings.data('book');
+  const oldRating = ratings.data('rating');
 
   if (oldRating === value) {
-    value = value - 0.5
+    value = value - 0.5;
   } else if (oldRating === value - 0.5) {
-    value = 0
+    value = 0;
   }
 
   $.ajax({
@@ -132,28 +134,28 @@ function rateBook () {
       xhr.setRequestHeader(
         'X-CSRFToken',
         String($('[name=csrfmiddlewaretoken]').val())
-      )
+      );
     },
     success: function () {
-      ratings.data('rating', value)
+      ratings.data('rating', value);
       ratings.children().each(function (i, star) {
         if (value >= i + 1) {
-          $(star).text('★')
+          $(star).text('★');
         } else if (value - i === 0.5) {
-          $(star).text('½')
+          $(star).text('½');
         } else {
-          $(star).text('☆')
+          $(star).text('☆');
         }
-      })
+      });
     }
-  })
+  });
 }
 
 /** @this Element */
 function removeTag () {
-  const tag = $(this).data('tag')
-  const label = $(this).parent()
-  const book = label.parent().data('book')
+  const tag = $(this).data('tag');
+  const label = $(this).parent();
+  const book = label.parent().data('book');
   $.ajax({
     type: 'POST',
     url: `/book/${book}/remove_tags/`,
@@ -162,23 +164,23 @@ function removeTag () {
       xhr.setRequestHeader(
         'X-CSRFToken',
         String($('[name=csrfmiddlewaretoken]').val())
-      )
+      );
       if (!confirm(`Remove tag ${tag}?`)) {
-        return false
+        return false;
       }
     },
     success: function () {
-      label.remove()
+      label.remove();
     }
-  })
+  });
 }
 
 /** @this Element, @param {Event} event */
 function updateProgress (event) {
-  event.preventDefault()
-  const form = $(this)
-  const url = form.attr('action')
-  const book = form.data('book')
+  event.preventDefault();
+  const form = $(this);
+  const url = form.attr('action');
+  const book = form.data('book');
 
   $.ajax({
     type: 'POST',
@@ -186,61 +188,61 @@ function updateProgress (event) {
     data: form.serialize(),
     dataType: 'json',
     success: function (data) {
-      const progressBar = $(`#book-${book} .progress-bar`)
-      progressBar.css('width', `${data.percentage}%`)
+      const progressBar = $(`#book-${book} .progress-bar`);
+      progressBar.css('width', `${data.percentage}%`);
 
-      let progressText = $(`#book-${book} .card-footer .progress-date`)
+      let progressText = $(`#book-${book} .card-footer .progress-date`);
       if (progressText.length === 0) {
-        progressText = $(`#book-${book} .card-footer .read-dates`)
-        progressText.text(`${progressText.text()}; ${data.progress_text}`)
+        progressText = $(`#book-${book} .card-footer .read-dates`);
+        progressText.text(`${progressText.text()}; ${data.progress_text}`);
       } else {
-        progressText.text(data.progress_text)
+        progressText.text(data.progress_text);
       }
 
-      form.find('input[name="value"]').val('')
+      form.find('input[name="value"]').val('');
     }
-  })
+  });
 }
 
 // eslint-disable-next-line no-unused-vars
 function updateScrollPosition () {
   $('h2').each(function () {
-    const height = $(window).height()
+    const height = $(window).height();
     if (typeof height === 'undefined') {
-      return
+      return;
     }
 
-    const top = window.pageYOffset
-    const offset = $(this).offset()
+    const top = window.pageYOffset;
+    const offset = $(this).offset();
     if (typeof offset === 'undefined') {
-      return
+      return;
     }
 
-    const distance = top - offset.top
-    const hash = $(this).attr('href')
+    const distance = top - offset.top;
+    const hash = $(this).attr('href');
 
     if (Math.abs(distance) < height * 0.75) {
       if (window.location.hash !== hash) {
         if (history.pushState) {
-          history.pushState(null, '', hash)
+          history.pushState(null, '', hash);
         } else if (typeof hash !== 'undefined') {
-          window.location.hash = hash
+          window.location.hash = hash;
         }
       }
     } else if (-distance > height && hash === window.location.hash) {
-      const year = hash.split('-').pop()
-      let newYear
+      const year = hash.split('-').pop();
+      let newYear;
       if (year === 'sometime') {
-        newYear = 2004
+        newYear = 2004;
       } else {
-        newYear = parseInt(String(year)) + 1
+        newYear = parseInt(String(year)) + 1;
       }
-      const newHash = `#read-${newYear}`
+      const newHash = `#read-${newYear}`;
       if (history.pushState) {
-        history.pushState(null, '', newHash)
+        history.pushState(null, '', newHash);
       } else {
-        window.location.hash = newHash
+        window.location.hash = newHash;
       }
     }
-  })
+  });
 }
