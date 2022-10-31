45a9171621295911bad19645975c2ddc9bc95584
Benjamin Eskola <ben@eskola.uk>
2022-10-31 18:09:13 +0000

    Rewrite loadNextPage as async

--- library/static/infinite_scroll.ts
+++ library/static/infinite_scroll.ts
@@ -3,52 +3,47 @@ import { RateBook } from './rate_book.js';
 import { TagBook } from './tag_book.js';
 
 function InfiniteScroll (): any {
-  function loadNextPage (year: string, url: string): void {
+  async function loadNextPage (year: string, url: string): Promise<void> {
     const placeholder = document.querySelector('.loader')!;
-    fetch(url)
-      .then(async response => {
-        if (!response.ok) {
-          throw new Error();
-        }
-        return await response.text();
-      })
-      .then(data => {
-        const elem = document.createElement('div');
-        elem.innerHTML = data.trim();
-        const body = elem.querySelector('main')!;
-        placeholder.parentElement?.insertAdjacentElement('afterend', body);
-        placeholder.remove();
+    const response = await fetch(url);
 
-        const progressBar = ProgressBar();
-        progressBar.init(body);
-        const rateBook = RateBook();
-        rateBook.init(body);
-        const tagBook = TagBook();
-        tagBook.init(body);
+    if (response.ok) {
+      const data = await response.text();
+      const elem = document.createElement('div');
+      elem.innerHTML = data.trim();
+      const body = elem.querySelector('main')!;
+      placeholder.parentElement?.insertAdjacentElement('afterend', body);
+      placeholder.remove();
 
-        if (year !== 'sometime') {
-          let nextYear = year;
-          if (Number(year) > 2010) {
-            nextYear = String(Number(year) - 1);
-          } else if (year === '2010') {
-            nextYear = '2007';
-          } else if (year === '2007') {
-            nextYear = '2004';
-          } else if (year === '2004') {
-            nextYear = 'sometime';
-          }
-          const nextUrl = url.replace(year, String(nextYear));
-          loadNextPage(nextYear, nextUrl);
+      const progressBar = ProgressBar();
+      progressBar.init(body);
+      const rateBook = RateBook();
+      rateBook.init(body);
+      const tagBook = TagBook();
+      tagBook.init(body);
+
+      if (year !== 'sometime') {
+        let nextYear = year;
+        if (Number(year) > 2010) {
+          nextYear = String(Number(year) - 1);
+        } else if (year === '2010') {
+          nextYear = '2007';
+        } else if (year === '2007') {
+          nextYear = '2004';
+        } else if (year === '2004') {
+          nextYear = 'sometime';
         }
-      })
-      .catch(() => {
-        (placeholder.querySelector(
-          '.spinner-grow'
-        ) as HTMLElement).style.display = 'none';
-        (placeholder.querySelector(
-          '.alert-danger'
-        ) as HTMLElement).style.display = '';
-      });
+        const nextUrl = url.replace(year, String(nextYear));
+        void loadNextPage(nextYear, nextUrl);
+      }
+    } else {
+      (placeholder.querySelector(
+        '.spinner-grow'
+      ) as HTMLElement).style.display = 'none';
+      (placeholder.querySelector(
+        '.alert-danger'
+      ) as HTMLElement).style.display = '';
+    }
   }
 
   function updateScrollPosition (this: HTMLElement): void {
