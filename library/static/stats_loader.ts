18e8841a0ec19894f7bb5e9af71bbeabaf6229b4
Benjamin Eskola <ben@eskola.uk>
2022-10-31 10:10:57 +0000

    Rewrite loadStatsForYear as async

--- library/static/stats_loader.ts
+++ library/static/stats_loader.ts
@@ -1,46 +1,37 @@
 function StatsLoader (): any {
-  function loadStatsForYear (element: HTMLElement): void {
+  async function loadStatsForYear (element: HTMLElement): Promise<void> {
     const year: string = element.dataset.year!;
 
-    fetch(`/stats/${year}`)
-      .then(async response => {
-        if (!response.ok) {
-          throw new Error();
-        } else {
-          const spinner: HTMLElement = element.querySelector('.spinner-grow')!;
-          spinner.style.display = '';
+    const response = await fetch(`/stats/${year}`);
+    if (response.ok) {
+      const spinner: HTMLElement = element.querySelector('.spinner-grow')!;
+      spinner.style.display = '';
+      element.innerHTML = await response.text();
+    } else {
+      element.innerHTML = `
+        <div id="error-${year}" class="alert alert-danger">
+          <i class="exclamation circle icon"></i>
+          <div class="content">
+            <div class="header">Failed to load ${year}.</div>
+            <p><span class="btn btn-outline-danger">Retry?</span></p>
+          </div>
+        </div>`;
 
-          return await response.text();
-        }
-      })
-      .then(html => {
-        element.innerHTML = html;
-      })
-      .catch(() => {
-        element.innerHTML = `
-              <div id="error-${year}" class="alert alert-danger">
-                <i class="exclamation circle icon"></i>
-                <div class="content">
-                  <div class="header">Failed to load ${year}.</div>
-                  <p><span class="btn btn-outline-danger">Retry?</span></p>
-                </div>
-              </div>`;
-
-        element
-          .querySelector(`#error-${year} .btn`)
-          ?.addEventListener('click', function () {
-            element.innerHTML = `
-              <div id="loading-stats-${year}" class="spinner-grow mb-4" role="status">
-                <span class="sr-only">Loading...</span>
-              </div>`;
-            return loadStatsForYear(element);
-          });
-      });
+      element
+        .querySelector(`#error-${year} .btn`)
+        ?.addEventListener('click', () => {
+          element.innerHTML = `
+            <div id="loading-stats-${year}" class="spinner-grow mb-4" role="status">
+              <span class="sr-only">Loading...</span>
+            </div>`;
+          void loadStatsForYear(element);
+        });
+    }
   }
 
   function init (): any {
     document.querySelectorAll('div.stats-for-year').forEach(el => {
-      loadStatsForYear(el as HTMLElement);
+      void loadStatsForYear(el as HTMLElement);
     });
   }
 
