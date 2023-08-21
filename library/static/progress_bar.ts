ea3a105024dc6aa3952d07fc3ce89ad153068fc5
Benjamin Eskola <ben@eskola.uk>
2023-08-21 12:45:09 +0100

    Mark as finished using AJAX

--- library/static/progress_bar.ts
+++ library/static/progress_bar.ts
@@ -1,4 +1,42 @@
 function ProgressBar(): any {
+  async function markFinished(
+    this: HTMLFormElement,
+    event: Event
+  ): Promise<any> {
+    event.preventDefault();
+    const book = this.dataset.book!;
+    const url = this.getAttribute('action')!;
+    console.log(book, url);
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams(new FormData(this) as any).toString()
+    });
+
+    if (response.ok) {
+      const bookCard = document.querySelector(`#book-${book}`)!;
+      const progressText: HTMLElement = bookCard.querySelector(
+        '.card-footer .progress-date'
+      )!;
+
+      const startField = bookCard.querySelector('.read-dates')!;
+      const startDate = startField.querySelector('time')!;
+      console.log(startDate);
+      startField.textContent = 'Read ';
+      startField.appendChild(startDate);
+      progressText.textContent = ' – now';
+
+      const progressBar = bookCard.querySelector('.progress')!;
+      progressBar.remove();
+      this.remove();
+      const toggle: HTMLElement = bookCard.querySelector('.dropdown-toggle')!;
+      toggle.click();
+    }
+  }
+
   async function updateProgress(
     this: HTMLFormElement,
     event: Event
@@ -22,7 +60,6 @@ function ProgressBar(): any {
       const progressBar: HTMLElement = bookCard.querySelector('.progress-bar')!;
       progressBar.style.width = `${data.percentage as string}%`;
 
-      console.log(data);
       let progressText: HTMLElement = bookCard.querySelector(
         '.card-footer .progress-date'
       )!;
@@ -35,7 +72,7 @@ function ProgressBar(): any {
           data.progress_text as string
         }`;
       } else {
-        progressText.textContent = data.progress_text;
+        progressText.textContent = `; ${data.progress_text}`;
       }
 
       (this.querySelector('input[name="value"]') as HTMLInputElement).value =
@@ -49,6 +86,11 @@ function ProgressBar(): any {
     body.querySelectorAll('form.update-progress').forEach((el) => {
       el.addEventListener('submit', (ev) => {
         void updateProgress.call(el as HTMLFormElement, ev);
+      });
+    });
+    body.querySelectorAll('form.finish').forEach((el) => {
+      el.addEventListener('submit', (ev) => {
+        void markFinished.call(el as HTMLFormElement, ev);
       });
     });
   }
