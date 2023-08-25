95f7827dc508d92778385ba9cef2a83325a72712
Benjamin Eskola <ben@eskola.uk>
2023-08-25 17:47:36 +0100

    Convert updateProgress function to Stimulus

--- library/javascript/controllers/book_controller.js
+++ library/javascript/controllers/book_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from '@hotwired/stimulus';
 
 export default class extends Controller {
-  static targets = ['ratings'];
+  static targets = ['ratings', 'updateForm'];
 
   displayRating(value) {
     this.ratingsTarget.dataset.rating = String(value);
@@ -41,5 +41,42 @@ export default class extends Controller {
     if (!response.ok) {
       this.displayRating(oldRating);
     }
+  }
+
+  async updateProgress(event) {
+    event.preventDefault();
+
+    const url = this.updateFormTarget.getAttribute('action');
+
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams(new FormData(this.updateFormTarget)).toString()
+    });
+
+    if (response.ok) {
+      const data = await response.json();
+      const progressBar = this.element.querySelector('.progress-bar');
+      progressBar.style.width = `${data.percentage}%`;
+
+      let progressText = this.element.querySelector(
+        '.card-footer .progress-date'
+      );
+      if (
+        progressText.textContent === null ||
+        progressText.textContent?.length === 0
+      ) {
+        progressText = this.element.querySelector('.card-footer .read-dates');
+        progressText.textContent = `${progressText.textContent}; ${data.progress_text}`;
+      } else {
+        progressText.textContent = `; ${data.progress_text}`;
+      }
+
+      this.updateFormTarget.querySelector('input[name="value"]').value = '';
+    }
+
+    return response;
   }
 }
--- library/static/progress_bar.ts
+++ library/static/progress_bar.ts
@@ -37,57 +37,7 @@ function ProgressBar(): any {
     }
   }
 
-  async function updateProgress(
-    this: HTMLFormElement,
-    event: Event
-  ): Promise<any> {
-    event.preventDefault();
-
-    const book = this.dataset.book!;
-    const url = this.getAttribute('action')!;
-
-    const response = await fetch(url, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: new URLSearchParams(new FormData(this) as any).toString()
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      const bookCard = document.querySelector(`#book-${book}`)!;
-      const progressBar: HTMLElement = bookCard.querySelector('.progress-bar')!;
-      progressBar.style.width = `${data.percentage as string}%`;
-
-      let progressText: HTMLElement = bookCard.querySelector(
-        '.card-footer .progress-date'
-      )!;
-      if (
-        progressText.textContent === null ||
-        progressText.textContent?.length === 0
-      ) {
-        progressText = bookCard.querySelector('.card-footer .read-dates')!;
-        progressText.textContent = `${progressText.textContent!}; ${
-          data.progress_text as string
-        }`;
-      } else {
-        progressText.textContent = `; ${data.progress_text}`;
-      }
-
-      (this.querySelector('input[name="value"]') as HTMLInputElement).value =
-        '';
-    }
-
-    return response;
-  }
-
   function init(body: HTMLElement): void {
-    body.querySelectorAll('form.update-progress').forEach((el) => {
-      el.addEventListener('submit', (ev) => {
-        void updateProgress.call(el as HTMLFormElement, ev);
-      });
-    });
     body.querySelectorAll('form.finish').forEach((el) => {
       el.addEventListener('submit', (ev) => {
         void markFinished.call(el as HTMLFormElement, ev);
